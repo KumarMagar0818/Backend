d598acc0e97ef7b5cdaacbe8ae31300ed5907bba
kumarmagar0818 <kumarmagar3400@gmail.com>
2023-12-05 18:32:40 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+    .then(() =>
+    {
+        app.listen(process.env.PORT || 8000, () =>
+        {
+            console.log(`Server is running at port :${ process.env.PORT }`);
+        })
+        // app.on("error", (error) =>
+        // {
+        //     console.log("Error: ", error);
+        //     throw error;
+        // })
+    })
+    .catch((error) =>
+    {
+        console.log("mongo db connection failed!!", error);
+    })
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,6 @@
+const asyncHandler = (requsetHandler) => (req, res, next) =>
+{
+    Promise.resolve(requsetHandler(req, res, next)).catch((err) => next(err))
+}
+
+export { asyncHandler }
